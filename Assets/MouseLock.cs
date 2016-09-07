49721df196da4131606e4cd566a03fbec6cd2862
Matthias <matthias.thurow@outlook.com>
2016-09-07 23:07:43 +0200

    added mouse lock during game

---
+++ Assets/MouseLock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+	}
+}
